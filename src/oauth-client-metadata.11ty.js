36a4f2915872670182ce03d03f1da94ec2f9b57f
Grace Kind <gracekind400@gmail.com>
2026-01-26 00:34:01 -0600

    Update oauth scopes

--- src/oauth-client-metadata.11ty.js
+++ src/oauth-client-metadata.11ty.js
@@ -12,7 +12,8 @@ export default {
       client_uri: `https://${data.hostName}`,
       logo_uri: `https://${data.hostName}/img/impro-logo.jpg`,
       redirect_uris: [`https://${data.hostName}/callback.html`],
-      scope: "atproto transition:generic transition:chat.bsky",
+      scope:
+        "atproto include:app.bsky.authFullApp include:chat.bsky.authFullChatClient",
       grant_types: ["authorization_code", "refresh_token"],
       response_types: ["code"],
       token_endpoint_auth_method: "none",
