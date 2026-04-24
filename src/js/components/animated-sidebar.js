adec8ca51d01951ede90056909c5da1d8cc81933
Grace Kind <gracekind400@gmail.com>
2026-04-24 15:02:31 -0500

    Convert animated sidebar to a dialog

--- src/js/components/animated-sidebar.js
+++ src/js/components/animated-sidebar.js
@@ -4,26 +4,46 @@ import { ScrollLock } from "/js/scrollLock.js";
 
 class AnimatedSidebar extends Component {
   connectedCallback() {
-    if (this._initialized) {
-      return;
+    if (!this._initialized) {
+      this.setAttribute("data-dialog-wrapper", "");
+      this.scrollLock = new ScrollLock(this);
+      this.isOpen = false;
+      this._children = getChildrenFragment(this);
+      this.innerHTML = "";
+      this.render();
+
+      this._mobileMediaQuery = window.matchMedia("(max-width: 799px)");
+      this._onViewportChange = (event) => {
+        if (!event.matches) {
+          this.close();
+        }
+      };
+
+      this._initialized = true;
     }
-    this.scrollLock = new ScrollLock(this);
-    this.isOpen = false;
-    this._children = getChildrenFragment(this);
-    this.innerHTML = "";
-    this.render();
-    this._initialized = true;
+    this._mobileMediaQuery.addEventListener("change", this._onViewportChange);
+  }
+
+  disconnectedCallback() {
+    this._mobileMediaQuery?.removeEventListener(
+      "change",
+      this._onViewportChange,
+    );
   }
 
   render() {
     render(
-      html`<div
-          class="sidebar-overlay ${this.isOpen ? "open" : ""}"
-          @click=${() => this.close()}
-        ></div>
-        <aside class="sidebar ${this.isOpen ? "open" : ""}">
-          <div class="sidebar-content"></div>
-        </aside>`,
+      html`<dialog
+        class="sidebar"
+        @click=${(event) => {
+          if (event.target.tagName === "DIALOG") {
+            this.close();
+          }
+        }}
+        @cancel=${() => this.close()}
+      >
+        <div class="sidebar-content"></div>
+      </dialog>`,
       this,
     );
     const sidebarContent = this.querySelector(".sidebar-content");
@@ -31,15 +51,27 @@ class AnimatedSidebar extends Component {
   }
 
   open() {
+    if (!window.matchMedia("(max-width: 799px)").matches) {
+      return;
+    }
+    if (this.isOpen) {
+      return;
+    }
     this.isOpen = true;
     this.scrollLock.lock();
-    this.render();
+    this.querySelector("dialog.sidebar").showModal();
   }
 
   close() {
+    if (!this.isOpen) {
+      return;
+    }
     this.isOpen = false;
     this.scrollLock.unlock();
-    this.render();
+    const dialog = this.querySelector("dialog.sidebar");
+    if (dialog.hasAttribute("open")) {
+      dialog.close();
+    }
   }
 }
 
