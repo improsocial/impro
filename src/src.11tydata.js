61154447049e2d1b8272bc843f8a4aacebdd4175
Grace Kind <gracekind400@gmail.com>
2026-06-06 00:38:23 -0500

    Import package version directly

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,12 +1,11 @@
 import { execSync } from "child_process";
 import { OAUTH_SCOPES } from "./oauthScopes.js";
+import pkg from "../package.json" with { type: "json" };
 
 export default {
   // Output the page as a HTML file
   permalink: (data) => (data.page.fileSlug || "index") + ".html",
-  version: execSync("node -p -e \"require('./package.json').version\"")
-    .toString()
-    .trim(),
+  version: pkg.version,
   gitCommit: () => execSync("git rev-parse --short=8 HEAD").toString().trim(),
   hostName: process.env.HOST_NAME ?? "dev.impro.social",
   environment: process.env.ENVIRONMENT ?? "development",
